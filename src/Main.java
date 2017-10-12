04acb613c72dfb3bb26e383c7cd5a58fb05ca3b7
Marcin Konieczny <marcin_k@coni.pl>
2017-10-12 13:07:58 +0200

    Version 3.0 z kontrolowanymi wyjątkami.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) throws IncorrectAgeException, NameUndefinedException, IncorrectPeselException {
 
         AddPerson.addPerson();
+
         System.out.println();
-
-
-
 
     }
 }
-
